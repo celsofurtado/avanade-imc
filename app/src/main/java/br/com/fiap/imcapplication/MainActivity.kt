c15625a391b202fa040592f52c306f4be115e2f9
Prof. Celso <celso.mfurtado@gmail.com>
2021-12-01 22:17:55 -0300

    Aula 01-12-2021

--- app/src/main/java/br/com/fiap/imcapplication/MainActivity.kt
+++ app/src/main/java/br/com/fiap/imcapplication/MainActivity.kt
@@ -2,10 +2,36 @@ package br.com.fiap.imcapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var editPeso: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        editPeso = findViewById(R.id.edit_peso)
+
+        val editAltura = findViewById<EditText>(R.id.edit_altura)
+        val btnCalcular = findViewById<Button>(R.id.btn_calcular)
+
+        btnCalcular.setOnClickListener {
+
+        }
+
+
+
+
     }
+
+    private fun calcularImc(peso: Int) {
+        val peso = editPeso.text.toString().toInt()
+    }
+
+
+
 }
